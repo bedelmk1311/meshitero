cb39de0a73db83e2a4d684cf7620f795345394d5
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-02-20 05:50:09 +0000

    [finish]4-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,9 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts.clear
+  config.hosts.clear #複数のホストからRailsアプリケーションにアクセス許可
+  
+  config.active_job.queue_adapter = :inline #エラー回避の対応をする?(4章-1)
+
 end
 
