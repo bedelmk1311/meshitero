8b916fb210564a65752cbfbd832c18ea51aa0477
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-02-20 06:59:30 +0000

    [finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  #deviseのUserモデルを追加した時に自動的に追加される
+
+
   root to: "homes#top"
 end
