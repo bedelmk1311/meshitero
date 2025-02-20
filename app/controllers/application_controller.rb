6cf66e35ac566586a8129b648858a69736118b4c
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-02-20 08:54:51 +0000

    [finish]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #deviseの機能が使われる前に、configure_permitted_parametersをする
+
+  protected #呼び出された他のコントローラからも参照することができます。
+
+  def configure_permitted_parameters #構成(configure)#許可された(permitted)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #サインアップ（:sign_up）時に、入力した内容（:name）を許可(permit)。
+  end
 end
