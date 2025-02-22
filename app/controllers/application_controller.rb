a911fc1d17d60955df21d172a408eccba128d227
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-02-22 12:31:34 +0000

    [finish]4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   #deviseの機能が使われる前に、configure_permitted_parametersをする
 
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
   protected #呼び出された他のコントローラからも参照することができます。
 
   def configure_permitted_parameters #構成(configure)#許可された(permitted)
