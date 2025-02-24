9675e064316974aae04cda076586e2cda8cfec89
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-02-24 08:11:42 +0000

    [finish]4-11 post_images/index

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,18 @@
 class PostImage < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :image 
+  #attach 付ける
   #postimageモデルでActiveStorageを使って画像投稿の機能を扱うために記述する
 
   belongs_to :user
   #  #アソシエーション1:NのN側にbelong_toを記載
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+  # get_imageメソッド
+  # unless もし〜でない限りは
 end
