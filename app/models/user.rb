830c65338a2136b73fbc40eca10bbdf36fbc42bb
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-01 14:54:24 +0000

    [finish]imagemagicのインストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
 
   has_many :post_images, dependent: :destroy
   #アソシエーション1:Nの1側にhas_manyを記載
+
+  has_one_attached :profile_image
+  #profile_imageという名前でActiveStorageでプロフィール画像を保存
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
+  #このメソッドを実行する際に100x100の画像にリサイズが行われる
+  #画像サイズの変更にImageMagick というライブラリが必要なので要インストール
 end
