dc9c4ac43851240a7fbdf37aa5af13e47b84af2e
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-02 06:22:24 +0000

    [finish]4-19

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+  def create
+    post_image = PostImage.find(params[:post_image_id])
+    favorite = current_user.favorites.new(post_image_id: post_image.id)
+    #引数がちんぷんかんぷん
+    #現在のユーザーのfavoritesテーブルを新規作成（引数はpost_imageのid）
+    favorite.save
+    redirect_to post_image_path(post_image)
+  end
+
+  def destroy
+    post_image = PostImage.find(params[:post_image_id])
+    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
+    favorite.destroy
+    redirect_to post_image_path(post_image)
+  end
 end
